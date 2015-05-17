7b2360ad442be541bd55c4d640487a556f7298f8
artur <chelaru.artur@mail.ru>
2015-05-18 01:51:35 +0300

    changed email sendTo

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	currencyList: ['USDJPY', 'EURUSD', 'AUDUSD', 'GBPUSD', 'EURJPY', 'CADUSD'],
 	//milliseconds
 	refreshTime: 5000,
-	sendTo: 'chelaru.artur@mail.ru',
+	sendTo: 'tokis_andersson@hotmail.com',
 	mail: {
 		user: 'chelaru.artur@gmail.com',
 		pass: 'dragan309'
